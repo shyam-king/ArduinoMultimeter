2b19740fc3ba1e81682f377c77e1a4b3fc0a3d34
Shyam <m.shyam.tnj@gmail.com>
2019-05-25 12:38:28 +0530

    implement document

--- Resistance/Resistance.ino
+++ Resistance/Resistance.ino
@@ -1,8 +1,12 @@
 /*
     AUTHOR: Shyam
     Domain: Embedded and Electronics
-    Functions: initPins, initADC, initTimers, readADC, main
-    Global variables: pin_charge, pin_capacitor, time_elapsed
+    Functions: initPins, initADC, readADC, initTimer1, startTimer1, stopTimer1, getMilliseconds,
+        initUSART, USART_sendByte, USART_sendData, calculateCapacitance, calculateResistance, checkMode,
+        updateLED, ISR (TIMER1_OVF_vect), main
+    Macros: PIN_LOW, PIN_HIGH
+    Global variables: pin_capacitance, pin_resistance, pin_voltage, pin_charge, pin_curent, pin_ts,
+        pin_led[4], timer1_overflow, mode
 */
 
 #include<avr/interrupt.h>
@@ -11,11 +15,23 @@
 #define PIN_LOW(port, pin) PORT##port &= !(1 << pin)
 #define PIN_HIGH(port, pin) PORT##port |= (1 << pin)
 
+#define MODE_IDLE 0
+#define MODE_CAPACITANCE 3
+#define MODE_RESISTANCE 2
+#define MODE_VOLTAGE 1
+#define MODE_CURRENT 4
 
-const int pin_capacitor = 0; // A0
-const int pin_charge = 2; // PORTD 
-float timer1_overflow; //to handle overflow of TCNT1
-float time_constant = 0; //callibrated timeConstant
+const int pin_capacitance = 0; // A0
+const int pin_resistance = 1; // A1
+const int pin_voltage = 2; // A2
+const int pin_charge = 4; // PORTB 
+const int pin_current = 3; //A3
+const int pin_ts = 5; //PORTB : On board led for trouble shooting
+
+const int pin_led[4] = {4,5,6,7}; //PORTD
+
+float timer1_overflow = 0; //to handle overflow of TCNT1
+uint8_t mode = MODE_IDLE;
 
 /*
     Function name: initPins
@@ -25,9 +41,22 @@ float time_constant = 0; //callibrated timeConstant
     Example call: initPins();
 */
 void initPins() {
-    DDRD |= (1 << pin_charge);
-    DDRC &= !(1 << pin_capacitor);
-    PIN_LOW(D, pin_charge);
+    //PORT C
+    DDRC = 0; //INPUT
+
+    //PORT B
+    DDRB |= (1 << pin_ts) | (1 << pin_charge); //OUTPUT
+
+    //PORTD 
+    DDRD |= (1 << pin_led[0]) | (1 << pin_led[1]) | (1 << pin_led[2]) | (1 << pin_led[3]); //OUTPUT
+
+    //PIN INITIAL OUTPUTS
+    PIN_LOW(B, pin_charge);
+    PIN_HIGH(B, pin_ts);
+    PIN_LOW(D, pin_led[0]);
+    PIN_LOW(D, pin_led[1]);
+    PIN_LOW(D, pin_led[2]);
+    PIN_LOW(D, pin_led[3]);
 }
 
 /*
@@ -62,15 +91,16 @@ uint16_t readADC(uint8_t pin)
 }
 
 /*
-    Function name: initTimers
+    Function name: initTimer1
     Input: none
     Output: none
     Logic: Initialize timers used: timer1 (16-bit)
-    Example call: initTimers();
+    Example call: initTimer1();
 */
-void initTimers() {
+void initTimer1() {
     TCCR1A = 0; //default configuration
     TIMSK1 |= 1; //overflow interrupt enable
+    timer1_overflow = 0;
 }
 
 /*
@@ -81,8 +111,8 @@ void initTimers() {
     Example call: startTimer1();
 */
 void startTimer1() {
+    initTimer1();
     TCCR1B = (1 << CS11) | (1 << CS10); //64 prescalar
-    timer1_overflow = 0;
     TCNT1 = 0;
 }
 
@@ -108,7 +138,8 @@ void stopTimer1() {
 */
 float getMilliseconds() {
     //250 ticks = 1 millisecond
-    return timer1_overflow + (float)TCNT1/250;
+    float t = timer1_overflow + (float)(TCNT1)/250.0f;
+    return t;
 }
 
 /*
@@ -185,6 +216,87 @@ void USART_sendData(uint16_t data) {
 }
 
 /*
+    Function name: calculateCapacitance
+    Input: none
+    Output: float : the capacitance of the capacitor connected
+    Logic: calculate the time to reach 0.63 Vcc == 648 ADC0 value and hence capacitance
+    Example call: float C = calculateCapacitance();
+*/
+float calculateCapacitance() {
+    //let the capacitor discharge
+    PIN_LOW(B, pin_charge); 
+    while (readADC(pin_capacitance) != 0);
+    
+    //calculate the time till capacitor is charged to 0.63Vcc (ADC reads 648)
+    startTimer1();
+    PIN_HIGH(B, pin_charge);
+    while(readADC(pin_capacitance) < 647);
+
+    float tc = getMilliseconds();
+    PIN_LOW(B, pin_charge);
+    stopTimer1();
+    return tc/10.0; //uF
+}
+
+/*
+    Function name: calculateResistance
+    Input: none
+    Output: float : the resistance connected
+    Logic: calculate the time to reach 0.63 Vcc == 648 ADC0 value and hence the resistance
+    Example call: float R = calculateResistance();
+*/
+float calculateResistance() {
+    //let the capacitor discharge
+    PIN_LOW(B, pin_charge); 
+    while (readADC(pin_resistance) != 0);
+    
+    //calculate the time till capacitor is charged to 0.63Vcc (ADC reads 648)
+    startTimer1();
+    PIN_HIGH(B, pin_charge);
+    while(1023 - readADC(pin_resistance) < 647);
+
+    float tc = getMilliseconds();
+    PIN_LOW(B, pin_charge);
+    stopTimer1();
+    return tc/100.0f; //kOhms
+}
+
+/*
+    Function name: checkMode
+    Input: none
+    Output: none
+    Logic: read input from the serial monitor and set the mode
+    Example call: checkMode();
+*/
+void checkMode() {
+    USART_sendData("SHYAM\'S MULTIMETER\n");
+    USART_sendData("KEY\t\tFUNCTION\n");
+    USART_sendData("1\t\tVOLTMETER\n");
+    USART_sendData("2\t\tRESISTANCE METER\n");
+    USART_sendData("3\t\tCAPACITANCE METER\n");
+    USART_sendData("4\t\tAMMETER\n");
+    while(!(UCSR0A & (1 << RXC0)));
+    mode = UDR0;
+    mode -= '0';
+}
+
+/*
+    Function name: updateLED
+    Input: none
+    Output: none
+    Logic: burn one of the four LEDs based on the input
+    Example call: checkMode();
+*/
+void updateLED() {
+    PIN_LOW(D, pin_led[0]);
+    PIN_LOW(D, pin_led[1]);
+    PIN_LOW(D, pin_led[2]);
+    PIN_LOW(D, pin_led[3]);
+    if(mode >= 1 && mode <= 4)
+        PIN_HIGH(D, pin_led[mode-1]);
+}
+
+/*
     Function name: ISR (TIMER1_OVF_vect)
     Input: none
     Output: none
@@ -193,51 +305,72 @@ void USART_sendData(uint16_t data) {
 */
 ISR (TIMER1_OVF_vect) {
     //250 ticks = 1 millisecond
-    timer1_overflow += (float)TCNT1/250;
+    timer1_overflow += (float)(0xFFFF)/250.0f;
 }
 
 /*
-    Function name: getTimeConstant
+    Function name: main
     Input: none
-    Output: float : the time constant
-    Logic: calculate the time to reach 0.63 Vcc == 645 ADC0 value
-    Example call: float tc = getTimeConstant();
-*/
-float getTimeConstant() {
-   // USART_sendData("Starting callibration\n");
-
-    //let the capacitor discharge
-    PIN_LOW(D, pin_charge); 
-    while (readADC(0) != 0);
-
-    //calculate the time till capacitor is charged to 0.63Vcc (ADC reads 645)
-    startTimer1();
-    PIN_HIGH(D, pin_charge);
-    while (readADC(0) < 645);
-    float tc = getMilliseconds();
-    stopTimer1();
-    PIN_LOW(D, pin_charge);
-
-    USART_sendData("Callibration complete\n");
-    return tc;
-}
-
+    Output: int : success/error code
+    Example call: <called implicitly>
+ */
 int main() {
     initPins();
     sei(); //enable global interrupts
-    initADC();
-    initTimers();
     initUSART();
+    initADC();
 
-    time_constant = getTimeConstant();
 
-    while (1) {
-        USART_sendData("Time constant: ");
-        USART_sendData(time_constant);
-        USART_sendByte('\n');
-        /*
-        uint16_t value = readADC(0);
-        USART_sendData(value);
-        USART_sendByte('\n');*/
+    checkMode();
+    updateLED();
+    USART_sendData("MODE: ");
+    
+    float value = 0;
+
+    switch(mode) {
+        case MODE_CAPACITANCE:
+            USART_sendData("Capacitance meter\n");
+            USART_sendData("Calculating time constant...\n");
+            value = calculateCapacitance();
+            USART_sendData("The capacitance is: ");
+            USART_sendData(value); 
+            USART_sendData(" uF\n");
+            USART_sendByte('\n');
+        break;
+
+        case MODE_RESISTANCE:
+            USART_sendData("Resistance meter\n");
+            USART_sendData("Calculating time constant...\n");
+            value = calculateResistance();
+            USART_sendData("The resistance is: ");
+            USART_sendData(value); 
+            USART_sendData(" k ohm\n");
+            USART_sendByte('\n');
+        break;
+
+        case MODE_VOLTAGE:
+            USART_sendData("Volt meter\n");
+            value = readADC(pin_voltage)*0.00488f;
+            USART_sendData("The Voltage is: ");
+            USART_sendData(value); 
+            USART_sendData(" V\n");
+            USART_sendByte('\n');
+        break;
+
+        case MODE_CURRENT:
+            USART_sendData("Ammeter\n");
+            value = readADC(pin_current)*0.488f;
+            USART_sendData("The current is: ");
+            USART_sendData(value); 
+            USART_sendData(" mA\n");
+            USART_sendByte('\n');
+        break;
+
+        default:
+            USART_sendData("Invalid\n");
     }
+
+    USART_sendData("\nRESET to reuse");
+
+    return 0;
 }
